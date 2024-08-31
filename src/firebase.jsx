7b69b8869947bbc08594322e8911bcba62b7b79f
MarcusVinici271 <131982502+MarcusVinici271@users.noreply.github.com>
2024-08-31 20:20:18 -0300

    Update firebase.jsx

--- src/firebase.jsx
+++ src/firebase.jsx
@@ -3,12 +3,12 @@ import { getAuth, GoogleAuthProvider, signInWithPopup, signOut } from 'firebase/
 import { getFirestore, collection, addDoc, getDocs, doc, deleteDoc } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyCEcjOyFql_C0hoF-vLCUQZlmi62st8VZA',
-  authDomain: 'notasreact-e640f.firebaseapp.com',
-  projectId: 'notasreact-e640f',
-  storageBucket: 'notasreact-e640f.appspot.com',
-  messagingSenderId: '458682913089',
-  appId: '1:458682913089:web:xxxxxxxxxxxxxxxxxxxxx',
+  apiKey: '',
+  authDomain: '',
+  projectId: '',
+  storageBucket: '',
+  messagingSenderId: '',
+  appId: '',
 };
 
 const app = initializeApp(firebaseConfig);
